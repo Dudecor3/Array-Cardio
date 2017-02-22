54b07808946f7d539a591bb42eee348390db0029
Marc Freeman <freeman.marc880@gmail.com>
2017-02-22 16:27:13 +0000

    Update array_cardio.js

--- array_cardio.js
+++ array_cardio.js
@@ -84,11 +84,15 @@ function sort_by_birthday(array) {
     }
 }
 
-// This function doesn't seem to be doing anything.
+
 const oldest = inventors.sort(function (a, b) {
     const lastInventor = a.passed - a.year;
     const nextInventor = b.passed - b.year;
-    return lastInventor > nextInventor ? -1 : 1; //Still need understanding of this ternary operator
+    if (lastInventor > nextInventor) {
+        return -1;
+    } else {
+        return 1;
+    }
 });
 
 
